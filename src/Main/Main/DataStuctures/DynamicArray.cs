557876cb5f8ee89a1c73d3fcbe516ee7f97fcf4b
BALAZS BERTA <95berta.balazs@gmail.com>
2023-04-02 12:43:23 +0200

    Init projects, Dynamic array init

---
+++ src/Main/Main/DataStuctures/DynamicArray.cs
@@ -0,0 +1,31 @@
+﻿namespace Main.DataStuctures
+{
+    public class DynamicArray<T>
+    {
+        private T[] _data;
+        private int _size;
+        private int _initialCapacity;
+
+        public DynamicArray(int initialCapacity)
+        {
+            _initialCapacity = initialCapacity;
+            _data = new T[initialCapacity];
+        }
+
+        public T this[int index]
+        {
+            get => GetValue(index);
+            set => SetValue(index, value);
+        }
+
+        public T GetValue(int index)
+        {
+            return _data[index];
+        }
+
+        public void SetValue(int index, T value)
+        {
+            _data[index] = value;
+        }
+    }
+}
