b916dce53afe4225c698957a749da16b0587d63d
BALAZS BERTA <95berta.balazs@gmail.com>
2023-04-02 14:17:59 +0200

    Added gh actions

---
+++ src/Main/DataStuctures/LinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.DataStuctures
+{
+    public class LinkedList<T>
+    {
+        private class Node<T>
+        {
+            public T Data { get; set; }
+            public Node<T> Next { get; set; }
+            public Node(T data)
+            {
+                Data = data;
+            }
+        }
+
+        private Node<T> _head;
+
+        public int Length()
+        {
+            if (_head == null) return 0;
+
+            var iterator = _head;
+            var size = 1;
+            while (iterator.Next != null)
+            {
+                size++;
+                iterator = iterator.Next;
+            }
+            return size;
+        }
+
+        public void AddFront(T data)
+        {
+            var newNode = new Node<T>(data);
+
+            if (_head == null)
+            {
+                _head = newNode;
+                return;
+            }
+
+            newNode.Next = _head;
+            _head = newNode;
+        }
+
+        public void AddBack(T data)
+        {
+            var newNode = new Node<T>(data);
+
+            if (_head == null)
+            {
+                _head = newNode;
+                return;
+            }
+
+            GetTail().Next = newNode;
+        }
+
+        public T GetFirst()
+        {
+            if (_head == null) throw new IndexOutOfRangeException("The Linked list is not filled.");
+
+            return _head.Data;
+        }
+
+        public T GetLast()
+        {
+            if (_head == null) throw new IndexOutOfRangeException("The Linked list is not filled.");
+
+            return GetTail().Data;
+        }
+
+        private Node<T> GetTail()
+        {
+            var iterator = _head;
+            while (iterator.Next != null)
+            {
+                iterator = iterator.Next;
+            }
+
+            return iterator;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+        }
+    }
+}
---
+++ src/Tests/DataStuctures/LinkedListTests.cs
@@ -0,0 +1,70 @@
+﻿using Main.DataStuctures;
+namespace Tests.DataStuctures
+{
+    public class LinkedListTests
+    {
+        public Main.DataStuctures.LinkedList<int> LinkedList { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            LinkedList = new Main.DataStuctures.LinkedList<int>();
+        }
+
+        [Test]
+        public void AddFrontTest()
+        {
+            LinkedList.AddFront(1);
+            LinkedList.AddFront(2);
+            LinkedList.AddFront(3);
+        }
+
+        [Test]
+        public void GetFirstTest()
+        {
+            LinkedList.AddFront(1);
+            Assert.That(LinkedList.GetFirst(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetLastTest()
+        {
+            LinkedList.AddFront(1);
+            LinkedList.AddFront(2);
+            LinkedList.AddFront(3);
+            Assert.That(LinkedList.GetLast(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddBackTest()
+        {
+            LinkedList.AddBack(1);
+            LinkedList.AddBack(2);
+            LinkedList.AddBack(3);
+            Assert.That(LinkedList.GetFirst(), Is.EqualTo(1));
+            Assert.That(LinkedList.GetLast(), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void SizeTest()
+        {
+            Assert.That(LinkedList.Length(), Is.EqualTo(0));
+            LinkedList.AddBack(1);
+            Assert.That(LinkedList.Length(), Is.EqualTo(1));
+            LinkedList.AddBack(2);
+            Assert.That(LinkedList.Length(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ClearTest()
+        {
+            LinkedList.AddBack(1);
+            LinkedList.AddBack(2);
+            LinkedList.AddBack(3);
+
+            LinkedList.Clear();
+
+            Assert.That(LinkedList.Length(), Is.EqualTo(0));
+        }
+    }
+}
