4fca73678a432a719370166b83d94614d7299612
BALAZS BERTA <95berta.balazs@gmail.com>
2023-04-15 13:05:49 +0200

    Simple bubblesort implementation

---
+++ src/Main/Algorithms/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Algorithms
+{
+    public static class BubbleSort
+    {
+        public static int[] Sort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 0; j < nums.Length - i -1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        var temp = nums[j]; 
+                        nums[j] = nums[j + 1]; 
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ src/Tests/Algorithms/BubbleSortTest.cs
@@ -0,0 +1,26 @@
+﻿using Main.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Algorithms
+{
+    public class BubbleSortTest
+    {
+        [Test]
+        public void Sort()
+        {
+            int[] array = { 5, 1, 4, 2, 8 };
+
+            int[] sorted = BubbleSort.Sort(array);
+
+            Assert.That(sorted[0], Is.EqualTo(1));
+            Assert.That(sorted[1], Is.EqualTo(2));
+            Assert.That(sorted[2], Is.EqualTo(4));
+            Assert.That(sorted[3], Is.EqualTo(5));
+            Assert.That(sorted[4], Is.EqualTo(8));
+        }
+    }
+}
